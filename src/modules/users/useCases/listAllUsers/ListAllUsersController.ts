1559b39b545313b5b30ff414bd99ad7bff558e87
Gus Cruz <gus.fr.cruz@gmail.com>
2022-02-04 23:06:29 -0300

    ✨ done

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,19 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    if (typeof user_id !== "string") {
+      throw new Error("User ID must be a string!");
+    }
+
+    try {
+      const all = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.status(200).json(all);
+    } catch {
+      return response.status(400).json({ error: "Ops! Something went wrong" });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,18 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    if (typeof user_id !== "string") {
+      throw new Error("User ID must be a string!");
+    }
+
+    try {
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+      return response.json(user);
+    } catch {
+      return response.status(404).json({ error: "User not found" });
+    }
   }
 }
 
